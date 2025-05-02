cfb5179ac471a704f0892bf14901a1ce13cc2da4
Abbas Khan <khanabbas161@gmail.com>
2025-05-03 01:40:24 +0530

    Code refactor

--- src/screens/homescreen/hooks/useScroll.ts
+++ src/screens/homescreen/hooks/useScroll.ts
@@ -5,7 +5,7 @@ import { SharedValue, withTiming } from 'react-native-reanimated';
 const useScroll = (scrollY: SharedValue<number>, scrollYGlobal: SharedValue<number>, prevScrollY: RefObject<number>) => {
     const onScroll = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
         const currentScrollY = e.nativeEvent.contentOffset.y;
-        const isScrollingDown = currentScrollY > prevScrollY.current;
+        const isScrollingDown = currentScrollY > 0 && currentScrollY > prevScrollY.current
         scrollY.value = withTiming(isScrollingDown ? 1 : 0, { duration: 300 });
         scrollYGlobal.value = currentScrollY;
         prevScrollY.current = currentScrollY;
